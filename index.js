294c300ff7d079b5b605f41ff0871fcedf696971
Mugure-Inez <enezmugure@gmail.com>
2021-11-30 16:54:31 +0300

    outlined expected features

---
+++ index.js
@@ -0,0 +1,11 @@
+// dependencies required for the app
+
+// render css files
+
+// placeholders for added tasks
+
+// post route for adding tasks
+
+// render the ejs and display added task, completed task
+
+// set app to listen on port 3000
